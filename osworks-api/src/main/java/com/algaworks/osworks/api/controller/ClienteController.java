78927fe1519d188e607c39faa761f3ade6c874a3
brunoemferreira <brunoemf@gmail.com>
2021-01-13 21:39:23 -0300

    Implementando verbo delete do cliente

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -9,6 +9,7 @@ import javax.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,6 +64,18 @@ public class ClienteController {
 		cliente = clienteRepository.save(cliente);
 		
 		return ResponseEntity.ok(cliente);
+	}
+	
+	// Usa-se o ResponseEntity para poder manipular o retorno dos status
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+		
+		if(!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(clienteId);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
